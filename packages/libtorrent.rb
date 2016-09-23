16d660ff292036e1695e19c1b83d7e9cd6a62a3c
Kristopher Ruzic <krruzic@gmail.com>
2016-09-23 00:13:48 -0600

    remove comments that got messed up and are unneeded

--- packages/libtorrent.rb
+++ packages/libtorrent.rb
@@ -14,20 +14,16 @@ class Libtorrent < Package # the package name
   source_url 'http://rtorrent.net/downloads/libtorrent-0.13.6.tar.gz'
   source_sha1 '493691929b701f4402f13c725840addf95231ad2' # software source sha1sum
 
-  depends_on 'buildessential automake m4 curl' # add build dependencies here
+  depends_on 'buildessential automake m4 curl'
   depends_on 'gcc'  
 
   def self.build                                                  # self.build 
-contains commands needed to build the software from source
     system "./configure --prefix=/usr/local"
     system "make"                                
   end
 
   def self.install                                                # self.install 
-contains commands needed to install the software on the target system
-    system "make", "DESTDIR=#{CREW_DEST_DIR}", "install"          # remember to 
-include DESTDIR set to CREW_DEST_DIR - needed to keep track of changes made to 
-system
+    system "make", "DESTDIR=#{CREW_DEST_DIR}", "install"         
   end         
 end
 
