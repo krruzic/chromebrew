822025e8e2b5da9b0844ebea2e2468d8af0c9de0
Kristopher Ruzic <krruzic@gmail.com>
2016-09-24 20:54:20 -0600

    add intltool (also installs cpanm to /home/chronos/user)

---
+++ packages/intltool.rb
@@ -0,0 +1,22 @@
+require 'package'
+
+class Intltool < Package
+  version '0.51.0'
+  source_url 'https://launchpad.net/intltool/trunk/0.51.0/+download/intltool-0.51.0.tar.gz'
+  source_sha1 'a0c3bcb99d1bcfc5db70f8d848232a47c47da090'
+
+  depends_on 'perl'
+
+  def self.build
+    system 'curl -L http://cpanmin.us | perl - App::cpanminus'
+    system '#{HOME}/perl5/bin/cpanm local::lib'
+    system '#{HOME}/perl5/bin/cpanm XML::Parser'
+    system './configure --prefix=/usr/local'
+    system "make"
+  end
+
+  def self.install
+    system "make", "DESTDIR=#{CREW_DEST_DIR}", "install"
+  end
+
+end
